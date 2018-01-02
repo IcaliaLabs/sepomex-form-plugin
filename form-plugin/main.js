dd178d70270e507d7a01ad421e603ab81c4efc22
Erik Torres <erik.torres.aguilar@gmail.com>
2018-01-02 17:10:14 -0600

    fixes several performance issues

--- form-plugin/main.js
+++ form-plugin/main.js
@@ -8,12 +8,17 @@ var axios = require('axios')
 (function( $ ) {
   $.fn.autocompleteForm = function(options) {
 
-    $.fn.autocompleteForm.defaults = {
-      suburbContainer: "#AddressSuburb",
-      cityContainer: "#AddressCity",
-      stateContainer: "#AddressState",
-      zipCodeContainer: "#AddressZipCode"
-    };
+    $.fn.sepomexForm.defaults = {
+    formContainer: "#AddressForm",
+    mapContainer: "#MapWrapper",
+    numberContainer: "#AddressNumber",
+    streetContainer: "#AddressStreet",
+    suburbContainer: "#AddressSuburb",
+    cityContainer: "#AddressCity",
+    stateContainer: "#AddressState",
+    countryContainer: "#AddressCountry",
+    zipCodeContainer: "#AddressZipCode"
+  };
 
     var opts = $.extend( {}, $.fn.autocompleteForm.defaults, options );
 
@@ -31,8 +36,14 @@ var axios = require('axios')
       Sepomex.where({zip_code: $($.fn.autocompleteForm.defaults.zipCodeContainer)[0].value, colony: $($.fn.autocompleteForm.defaults.suburbContainer)[0].value, city:  $($.fn.autocompleteForm.defaults.cityContainer)[0].value}).then((response) => {
           completeCity(response);
           completeState(response);
+          $("input[type='text']").trigger("change");
         }).catch((error) => {
-          console.log("Error in autocompleting zipcode")
+          $($.fn.sepomexForm.defaults.formContainer + ' :input').each(function() {
+            if(this != $.fn.sepomexForm.defaults.zipCodeContainer ){
+                this.val("");
+              }
+          });
+          console.log("Error in autocompleting zipcode");
       });
     });
 
@@ -41,16 +52,18 @@ var axios = require('axios')
           completeZipCode(response)
           completeCity(response);
           completeState(response);
+          $("input[type='text']").trigger("change");
         }).catch((error) => {
-          console.log("Error in autocompleting suburb")
+          console.log("Error in autocompleting suburb");
       });
     });
 
     $($.fn.autocompleteForm.defaults.cityContainer).focusout(function(){
       Sepomex.where({zip_code: $($.fn.autocompleteForm.defaults.zipCodeContainer)[0].value, colony: $($.fn.autocompleteForm.defaults.suburbContainer)[0].value, city:  $($.fn.autocompleteForm.defaults.cityContainer)[0].value}).then((response) => {
           completeState(response);
+          $("input[type='text']").trigger("change");
         }).catch((error) => {
-          console.log("Error in autocompleting city")
+          console.log("Error in autocompleting city");
       });
     });
   }
@@ -96,7 +109,7 @@ var axios = require('axios')
               else if(address_components[i].types.includes('locality')) {
                 $($.fn.sepomexForm.defaults.cityContainer).val(address_components[i].long_name);
               }
-              else if(address_components[i].types.includes('administrative_area_level_2')) {
+              else if(address_components[i].types.includes('administrative_area_level_1')) {
                 $($.fn.sepomexForm.defaults.stateContainer).val(address_components[i].long_name);
               }
               else if(address_components[i].types.includes('country')) {
@@ -134,7 +147,7 @@ var axios = require('axios')
       return address;
     }
 
-    $($.fn.sepomexForm.defaults.formContainer).focusout(function(){
+    $("input[type='text']").change(function(){
       getLatitudeLongitudeAndSetInMap(function(latLng) {
         map.setCenter(latLng);
         marker.setVisible(true);
